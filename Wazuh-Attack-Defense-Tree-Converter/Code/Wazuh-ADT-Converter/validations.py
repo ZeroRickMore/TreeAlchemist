36661badc1a3fb4259c184c577506b6f6c6f3e99
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-10-28 00:10:47 +0100

    Implement IP address validation

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/validations.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/validations.py
@@ -3,15 +3,53 @@
 # ============================================
 
 def is_allowed(allowed_values : list, string : str) -> bool:
+    '''
+    Check if the given string is one of the ones in allowed_values list
+    '''
     for allowed_value in allowed_values:
         if string == allowed_value:
             return True
     return False 
 
 def is_yes_or_no(string : str) -> bool:
+    '''
+    Check if the given string is one of ["yes", "no"]
+    '''
     allowed_values = ["yes", "no"]
     return is_allowed(allowed_values=allowed_values, string=string)
 
 def is_osmatch_osregex_pcre2(string : str) -> bool:
+    '''
+    Check if the given string is one of ["osmatch", "osregex", "pcre2"]
+    '''
     allowed_values = ["osmatch", "osregex", "pcre2"]
     return is_allowed(allowed_values=allowed_values, string=string)
+
+
+def is_ip_address(string : str) -> bool:
+    '''
+    Check if the given string is a valid IPv4 or IPv6 address
+    '''
+    return is_valid_ipv4(ip = string) or is_valid_ipv6(ip = string)
+
+import ipaddress
+
+def is_valid_ipv4(ip : str) -> bool:
+    try:
+        # Try to create an IPv4 address object
+        ip = ipaddress.IPv4Address(ip)
+        return True
+    except ipaddress.AddressValueError:
+        # If an exception is raised, the address is not valid
+        return False
+
+
+def is_valid_ipv6(ip : str) -> bool:
+    try:
+        # Try to create an IPv6 address object
+        ip = ipaddress.IPv6Address(ip)
+        return True
+    except ipaddress.AddressValueError:
+        # If an exception is raised, the address is not valid
+        return False
+
