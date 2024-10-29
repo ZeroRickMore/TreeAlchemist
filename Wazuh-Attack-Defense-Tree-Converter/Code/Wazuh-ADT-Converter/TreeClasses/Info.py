d40e852ae0cd6bb791781307b80c868bf6a6571e
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-10-30 00:10:40 +0100

    Improve validations

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/Info.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/Info.py
@@ -35,17 +35,24 @@ class Info:
         allowed_values=["text", "cve", "link", "ovsdb"]
         return validations.is_allowed(allowed_values=allowed_values, string=self.get_wrc_info_type())
 
+    def validate_wrc_info_type_with_error_launch(self):
+        if not self.validate_wrc_info_type():
+            error_prefix = f"The node {self.relative_node_name} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f'{error_prefix} <freq_info type="{self.get_wrc_info_type()}"> in <wazuh_rule_config>. {Info.get_wrc_info_type_allow_criteria()} {self.get_wrc_info_type()} of type {type(self.get_wrc_info_type())} {error_suffix}')
+        
+
     @staticmethod
     def get_wrc_info_type_allow_criteria():
         return "It must be 'text' or 'cve' or 'link' or 'ovsdb'."
 
     def set_wrc_info_type(self, type_ : str):
         self.type = type_
-        if self.validate_wrc_info_type():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <info type=""> of node {self.relative_node_name} has been succesfully set to {type_}')
-        else:
-            ExitUtils.exit_with_error(f'You cannot set <info type=""> of node {self.relative_node_name} to {type_} of type {type(type_)}. {Info.get_wrc_info_type_allow_criteria()}')
+
+        self.validate_wrc_info_type_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <info type=""> of node {self.relative_node_name} has been succesfully set to {type_}')
 
 
     # ============================================
@@ -61,30 +68,35 @@ class Info:
             return False
         return True
 
+    def validate_wrc_info_info_with_error_launch(self):
+        if not self.validate_wrc_info_info():
+            error_prefix = f"The node {self.relative_node_name} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f'{error_prefix} <freq_info>{self.get_wrc_info_info()}</freq_info> in <wazuh_rule_config>. {Info.get_wrc_info_info_allow_criteria()} {self.get_wrc_info_info()} of type {type(self.get_wrc_info_info())} {error_suffix}')
+        
+
     @staticmethod
     def get_wrc_info_info_allow_criteria():
         return "It must be a string."
 
     def set_wrc_info_info(self, info : str):
         self.info = info
-        if self.validate_wrc_info_info():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- Inside <wazuh_rule_config>, <info> of node {self.relative_node_name} has been succesfully set to {info}")
-        else:
-            ExitUtils.exit_with_error(f"You cannot set <info> of node {self.relative_node_name} to {info} of type {type(info)}. {Info.get_wrc_info_info_allow_criteria()}")
+
+        self.validate_wrc_info_info_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- Inside <wazuh_rule_config>, <info> of node {self.relative_node_name} has been succesfully set to {info}")
 
     # ============================================
     # Validate All
     # ============================================
 
     def validate_all(self):
-        error_prefix = f"The node {self.relative_node_name} failed validation on"
-        error_suffix = f"was given instead."
-
-        if not self.validate_wrc_info_type():
-            ExitUtils.exit_with_error(f'{error_prefix} <freq_info type="{self.get_wrc_info_type()}"> in <wazuh_rule_config>. {Info.get_wrc_info_type_allow_criteria()} {self.get_wrc_info_type()} of type {type(self.get_wrc_info_type())} {error_suffix}')
-        if not self.validate_wrc_info_info():
-            ExitUtils.exit_with_error(f'{error_prefix} <freq_info>{self.get_wrc_info_info()}</freq_info> in <wazuh_rule_config>. {Info.get_wrc_info_info_allow_criteria()} {self.get_wrc_info_info()} of type {type(self.get_wrc_info_info())} {error_suffix}')
+        
+        self.validate_wrc_info_type_with_error_launch()
+            
+        self.validate_wrc_info_info_with_error_launch()
+            
         #PrintUtils.print_in_green(f"- Validation of a <freq_info> related to {self.relative_node_name} was succesful!")
 
     # ============================================
