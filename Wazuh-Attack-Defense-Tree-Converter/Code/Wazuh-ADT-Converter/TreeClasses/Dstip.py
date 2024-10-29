6bb612e601949b14f46a6fcb78b4e60e39405a89
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-10-29 23:51:55 +0100

    Improve validations

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/Dstip.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/Dstip.py
@@ -33,18 +33,24 @@ class Dstip:
         # Allowed values check
         return validations.is_yes_or_no(self.get_wrc_dstip_negate())
 
+    def validate_wrc_dstip_negate_with_error_launch(self):
+        if not self.validate_wrc_dstip_negate():
+            error_prefix = f"The node {self.relative_node_name} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f'{error_prefix} <dstip negate="{self.get_wrc_dstip_negate()}"> in <wazuh_rule_config>. {Dstip.get_wrc_dstip_negate_allow_criteria()} {self.get_wrc_dstip_negate()} of type {type(self.get_wrc_dstip_negate())} {error_suffix}')
+        
     @staticmethod
     def get_wrc_dstip_negate_allow_criteria():
         return "It must be 'yes' or 'no'."
 
     def set_wrc_dstip_negate(self, negate : str):
         self.negate = negate
-        if self.validate_wrc_dstip_negate():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <dstip negate=""> of node {self.relative_node_name} has been succesfully set to {negate}')
-        else:
-            ExitUtils.exit_with_error(f'You cannot set <dstip negate=""> of node {self.relative_node_name} to {negate} of type {type(negate)}. {Dstip.get_wrc_dstip_negate_allow_criteria()}')
 
+        self.validate_wrc_dstip_negate_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <dstip negate=""> of node {self.relative_node_name} has been succesfully set to {negate}')
+        
 
     # ============================================
     # <dstip> operations
@@ -60,30 +66,36 @@ class Dstip:
         # Allowed values check
         return validations.is_ip_address(string=self.get_wrc_dstip_dstip())
 
+    def validate_wrc_dstip_dstip_with_error_launch(self):
+        if not self.validate_wrc_dstip_dstip():
+            error_prefix = f"The node {self.relative_node_name} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f'{error_prefix} <dstip> in <wazuh_rule_config>. {Dstip.get_wrc_dstip_dstip_allow_criteria()} {self.get_wrc_dstip_dstip()} of type {type(self.get_wrc_dstip_dstip())} {error_suffix}')
+        
+
     @staticmethod
     def get_wrc_dstip_dstip_allow_criteria():
         return "It must be a valid IPv4 or IPv6 ip address, given as a string."
 
     def set_wrc_dstip_dstip(self, dstip : str):
         self.dstip = dstip
-        if self.validate_wrc_dstip_dstip():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <dstip> of node {self.relative_node_name} has been succesfully set to {dstip}')
-        else:
-            ExitUtils.exit_with_error(f'You cannot set <dstip> of node {self.relative_node_name} to {dstip} of type {type(dstip)}. {Dstip.get_wrc_dstip_dstip_allow_criteria()}')
+
+        self.validate_wrc_dstip_dstip_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f'- Inside <wazuh_rule_config>, <dstip> of node {self.relative_node_name} has been succesfully set to {dstip}')
+
 
     # ============================================
     # Validate All
     # ============================================
 
     def validate_all(self):
-        error_prefix = f"The node {self.relative_node_name} failed validation on"
-        error_suffix = f"was given instead."
 
-        if not self.validate_wrc_dstip_negate():
-            ExitUtils.exit_with_error(f'{error_prefix} <dstip negate="{self.get_wrc_dstip_negate()}"> in <wazuh_rule_config>. {Dstip.get_wrc_dstip_negate_allow_criteria()} {self.get_wrc_dstip_negate()} of type {type(self.get_wrc_dstip_negate())} {error_suffix}')
-        if not self.validate_wrc_dstip_dstip():
-            ExitUtils.exit_with_error(f'{error_prefix} <dstip> in <wazuh_rule_config>. {Dstip.get_wrc_dstip_dstip_allow_criteria()} {self.get_wrc_dstip_dstip()} of type {type(self.get_wrc_dstip_dstip())} {error_suffix}')
+        self.validate_wrc_dstip_negate_with_error_launch()
+            
+        self.validate_wrc_dstip_dstip_with_error_launch()
+            
         #PrintUtils.print_in_green(f"- Validation of a <regex> related to {self.relative_node_name} was succesful!")
 
     # ============================================
