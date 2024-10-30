756d4a81a77258a1399b6a7441e406919a4d38e9
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-10-30 08:46:39 +0100

    Fix wazuh_rule_config object handling

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/TreeNodeInformations.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/TreeClasses/TreeNodeInformations.py
@@ -35,7 +35,7 @@ class TreeNodeInformations:
                 path                    : str = "", # MUST be changed to a valid path, so it must end with a /. It's up to you to build it correctly, obviously!
                 id                      : int = -1, # MUST be changed
                 name                    : str = "undefined", # SHOULD be changed
-                wazuh_rule_config       : WazuhRuleConfig = None, # MUST be changed
+                wazuh_rule_config       : WazuhRuleConfig = WazuhRuleConfig(), # MUST be changed
                 ):
         
         # <node conjuncted_children="" root="" type=""
@@ -69,17 +69,23 @@ class TreeNodeInformations:
         # Allowed values check
         return validations.is_yes_or_no(self.get_conjuncted_children())
     
+    def validate_node_conjuncted_children_with_error_launch(self):
+        if not self.validate_node_conjuncted_children():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} node attribute [conjuncted_children]. {TreeNodeInformations.get_conjuncted_children_allow_criteria()} {self.get_conjuncted_children()} of type {type(self.get_conjuncted_children())} {error_suffix}")
+
     @staticmethod
     def get_conjuncted_children_allow_criteria() -> str:
         return "It must be 'yes' or 'no'."
 
     def set_conjuncted_children(self, conjuncted_children : str):
         self.node_tag_attributes["conjuncted_children"] = conjuncted_children
-        if self.validate_node_conjuncted_children():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- conjuncted_children of node {self.get_name()} with id {self.get_id()} has been succesfully set to {conjuncted_children}")
-        else:
-            ExitUtils.exit_with_error(f"You cannot set conjuncted_children of node {self.get_name()} with id {self.get_id()} to {conjuncted_children}  of type {type(conjuncted_children)}. {TreeNodeInformations.get_conjuncted_children_allow_criteria()}")
+
+        self.validate_node_conjuncted_children_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- conjuncted_children of node {self.get_name()} with id {self.get_id()} has been succesfully set to {conjuncted_children}")
 
     # ============================================
     # <node root=""> operations
@@ -95,18 +101,25 @@ class TreeNodeInformations:
         # Allowed values check
         return validations.is_yes_or_no(self.node_tag_attributes["root"])
 
+    def validate_node_root_with_error_launch(self):
+        if not self.validate_node_root():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} node attribute [root]. {TreeNodeInformations.get_root_allow_criteria()} {self.get_root()} of type {type(self.get_root())} {error_suffix}")
+
+
     @staticmethod
     def get_root_allow_criteria() -> str:
         return "It must be 'yes' or 'no'."
 
     def set_root(self, root : str):
         self.node_tag_attributes["root"] = root
-        if self.validate_node_root():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- root of node {self.get_name()} with id {self.get_id()} has been succesfully set to {root}")
-        else:
-            ExitUtils.exit_with_error(f'You cannot set <node root=""> of node {self.get_name()} with id {self.get_id()} to {root} of type {type(root)}. {TreeNodeInformations.get_root_allow_criteria()}')
-    
+
+        self.validate_node_root_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- root of node {self.get_name()} with id {self.get_id()} has been succesfully set to {root}")
+
 
     # ============================================
     # <node type=""> operations
@@ -123,19 +136,23 @@ class TreeNodeInformations:
         allowed_values = ["atk", "def"]
         return validations.is_allowed(allowed_values=allowed_values, string=self.get_type())
 
+    def validate_node_type_with_error_launch(self):
+        if not self.validate_node_type():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} node attribute [type]. {TreeNodeInformations.get_type_allow_criteria()} {self.get_type()} of type {type(self.get_type())} {error_suffix}")
+
     @staticmethod
     def get_type_allow_criteria() -> str:
         return "It must be 'atk' or 'def'."
 
     def set_type(self, type_ : str):
         self.node_tag_attributes["type"] = type_
-        if self.validate_node_type():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- type of node {self.get_name()} with id {self.get_id()} has been succesfully set to {type_}")
-        else:
-            ExitUtils.exit_with_error(f'You cannot set <node type="" of node {self.get_name()} with id {self.get_id()} to {type_} of type {type(type_)}. {TreeNodeInformations.get_type_allow_criteria()}')
 
+        self.validate_node_type_with_error_launch()
 
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- type of node {self.get_name()} with id {self.get_id()} has been succesfully set to {type_}")
 
     # ============================================
     # <path> operations
@@ -152,17 +169,23 @@ class TreeNodeInformations:
         # Check if it ends with a /, basically that's the check we need. The existence of the path cannot be checked here.
         return self.get_path().endswith(r"/")
 
+    def validate_path_with_error_launch(self):
+        if not self.validate_path():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} <path>. {TreeNodeInformations.get_path_allow_criteria()} {self.get_path()} of type {type(self.get_path())} {error_suffix}")
+
     @staticmethod
     def get_path_allow_criteria() -> str:
         return "It must end with a / ."
 
     def set_path(self, path : str):
         self.path = path
-        if self.validate_path():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- <path> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {path}")
-        else:
-            ExitUtils.exit_with_error(f"You cannot set <path> of node {self.get_name()} with id {self.get_id()} to {path} of type {type(path)}. {TreeNodeInformations.get_path_allow_criteria()}")
+    
+        self.validate_path_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- <path> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {path}")
 
 
     # ============================================
@@ -175,17 +198,23 @@ class TreeNodeInformations:
     def validate_id(self) -> bool:
         return isinstance(self.get_id(), int) and self.get_id() >= 0
 
+    def validate_id_with_error_launch(self):
+        if not self.validate_id():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} <id>. {TreeNodeInformations.get_id_allow_criteria()} {self.get_id()} of type {type(self.get_id())} {error_suffix}")
+        
     @staticmethod
     def get_id_allow_criteria() -> str:
         return "It must be a positive integer."
 
     def set_id(self, id : int):
         self.id = id
-        if self.validate_id():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- <id> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {id}")
-        else:
-            ExitUtils.exit_with_error(f"You cannot set <id> of node {self.get_name()} with id {self.get_id()} to {id} of type {type(id)}. {TreeNodeInformations.get_id_allow_criteria()}")
+
+        self.validate_id_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- <id> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {id}")
 
 
     # ============================================
@@ -199,18 +228,23 @@ class TreeNodeInformations:
         # Type check
         return isinstance(self.get_name(), str)
 
+    def validate_name_with_error_launch(self):
+        if not self.validate_name():
+            error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f"{error_prefix} <name>. {TreeNodeInformations.get_name_allow_criteria()} {self.get_name()} of type {type(self.get_name())} {error_suffix}")
+
     @staticmethod
     def get_name_allow_criteria() -> str:
         return "It must be a string."
 
     def set_name(self, name : str):
         self.name = name
-        if self.validate_name():
-            if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- <name> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {name}")
-        else:
-            ExitUtils.exit_with_error(f"You cannot set <name> of node {self.get_name()} with id {self.get_id()} to {name} of type {type(name)}. {TreeNodeInformations.get_name_allow_criteria()}")
 
+        self.validate_name_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f"- <name> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {name}")
 
 
     # ============================================
@@ -226,15 +260,11 @@ class TreeNodeInformations:
         self.get_wazuh_rule_config().validate_all()
         return True    
 
-    @staticmethod
-    def get_wrc_options_allow_criteria() -> str:
-        return 'Every <options> tag must contain one of the following strings: "alert_by_email", "no_email_alert", "no_log", "no_full_log", "no_counter".'
-
-    def set_wrc_options(self, options : str):
-        self.wazuh_rule_config["options"] = options
-        if self.validate_wrc_options():
+    def set_wazuh_rule_config(self, wrc : WazuhRuleConfig):
+        self.wazuh_rule_config = wrc
+        if self.validate_wazuh_rule_config():
             if self.print_diagnostics:
-                PrintUtils.print_in_green(f"- Inside <wazuh_rule_config>, <options> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {options}")
+                PrintUtils.print_in_green(f"- <wazuh_rule_config> of node {self.get_name()} with id {self.get_id()} has been succesfully set to {wrc}")
         # else: is covered inside of self.validate_wrc_options() already
 
 
@@ -247,85 +277,21 @@ class TreeNodeInformations:
     # ============================================   
 
     def validate_all(self) -> bool:
-        error_prefix = f"The node {self.get_name()} with id {self.get_id()} failed validation on"
-        error_suffix = f"was given instead."
 
-        if not self.validate_node_conjuncted_children():
-            ExitUtils.exit_with_error(f"{error_prefix} node attribute [conjuncted_children]. {TreeNodeInformations.get_conjuncted_children_allow_criteria()} {self.get_conjuncted_children()} of type {type(self.get_conjuncted_children())} {error_suffix}")
-        
-        if not self.validate_node_root():
-            ExitUtils.exit_with_error(f"{error_prefix} node attribute [root]. {TreeNodeInformations.get_root_allow_criteria()} {self.get_root()} of type {type(self.get_root())} {error_suffix}")
-        
-        if not self.validate_node_type():
-            ExitUtils.exit_with_error(f"{error_prefix} node attribute [type]. {TreeNodeInformations.get_type_allow_criteria()} {self.get_type()} of type {type(self.get_type())} {error_suffix}")
-        
-        if not self.validate_path():
-            ExitUtils.exit_with_error(f"{error_prefix} <path>. {TreeNodeInformations.get_path_allow_criteria()} {self.get_path()} of type {type(self.get_path())} {error_suffix}")
-        
-        if not self.validate_id():
-            ExitUtils.exit_with_error(f"{error_prefix} <id>. {TreeNodeInformations.get_id_allow_criteria()} {self.get_id()} of type {type(self.get_id())} {error_suffix}")
-        
-        if not self.validate_name():
-            ExitUtils.exit_with_error(f"{error_prefix} <name>. {TreeNodeInformations.get_name_allow_criteria()} {self.get_name()} of type {type(self.get_name())} {error_suffix}")
-        
-        if not self.validate_wrc_frequency():
-            ExitUtils.exit_with_error(f"{error_prefix} <frequency> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_frequency_allow_criteria()} {self.get_wrc_frequency()} of type {type(self.get_wrc_frequency())} {error_suffix}")
-        
-        if not self.validate_wrc_timeframe():
-            ExitUtils.exit_with_error(f"{error_prefix} <timeframe> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_timeframe_allow_criteria()} {self.get_wrc_timeframe()} of type {type(self.get_wrc_timeframe())} {error_suffix}")
-        
-        if not self.validate_wrc_ignore():
-        
-            ExitUtils.exit_with_error(f"{error_prefix} <ignore_after> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_ignore_allow_criteria()} {self.get_wrc_ignore()} of type {type(self.get_wrc_ignore())} {error_suffix}")
-        
-        if not self.validate_wrc_already_existing_id():
-            ExitUtils.exit_with_error(f"{error_prefix} <already_existing_id> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_already_existing_id_allow_criteria()} {self.get_wrc_already_existing_id()} of type {type(self.get_wrc_already_existing_id())} {error_suffix}")
-        
-        self.validate_wrc_match()
-
-        self.validate_wrc_regex()
-
-        self.validate_wrc_srcip()
-
-        self.validate_wrc_dstip()
-
-        self.validate_wrc_srcport()
-
-        self.validate_wrc_dstport()
-
-        if not self.validate_wrc_time():
-            ExitUtils.exit_with_error(f"{error_prefix} <time> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_time_allow_criteria()} {self.get_wrc_time()} of type {type(self.get_wrc_time())} {error_suffix}")
-
-        if not self.validate_wrc_weekday():
-            ExitUtils.exit_with_error(f"{error_prefix} <weekday> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_weekday_allow_criteria()} {self.get_wrc_weekday()} of type {type(self.get_wrc_weekday())} {error_suffix}")
-
-        if not self.validate_wrc_same_srcip():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_same_srcip> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_same_srcip_allow_criteria()} {self.get_wrc_same_srcip()} of type {type(self.get_wrc_same_srcip())} {error_suffix}")
-  
-        if not self.validate_wrc_different_srcip():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_different_srcip> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_different_srcip_allow_criteria()} {self.get_wrc_different_srcip()} of type {type(self.get_wrc_different_srcip())} {error_suffix}")
-
-        if not self.validate_wrc_same_srcport():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_same_srcport> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_same_srcport_allow_criteria()} {self.get_wrc_same_srcport()} of type {type(self.get_wrc_same_srcport())} {error_suffix}")
-    
-        if not self.validate_wrc_different_srcport():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_different_srcport> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_different_srcport_allow_criteria()} {self.get_wrc_different_srcport()} of type {type(self.get_wrc_different_srcport())} {error_suffix}")
-   
-        if not self.validate_wrc_same_location():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_same_location> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_same_location_allow_criteria()} {self.get_wrc_same_location()} of type {type(self.get_wrc_same_location())} {error_suffix}")
-   
-        if not self.validate_wrc_same_srcuser():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_same_srcuser> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_same_srcuser_allow_criteria()} {self.get_wrc_same_srcuser()} of type {type(self.get_wrc_same_srcuser())} {error_suffix}")
-   
-        if not self.validate_wrc_different_srcuser():
-            ExitUtils.exit_with_error(f"{error_prefix} <freq_different_srcuser> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_different_srcuser_allow_criteria()} {self.get_wrc_different_srcuser()} of type {type(self.get_wrc_different_srcuser())} {error_suffix}")
-   
-        if not self.validate_wrc_description():
-            ExitUtils.exit_with_error(f"{error_prefix} <description> in <wazuh_rule_config>. {TreeNodeInformations.get_wrc_description_allow_criteria()} {self.get_wrc_description()} of type {type(self.get_wrc_description())} {error_suffix}")
-   
-        self.validate_wrc_info()
-
-        self.validate_wrc_options()
+        self.validate_node_conjuncted_children_with_error_launch()
+                    
+        self.validate_node_root_with_error_launch()
+                    
+        self.validate_node_type_with_error_launch()
+            
+        self.validate_path_with_error_launch()
+            
+        self.validate_id_with_error_launch()
+            
+        self.validate_name_with_error_launch()
+            
+        self.validate_wazuh_rule_config()
+            
 
     # ============================================
     # General to_string
