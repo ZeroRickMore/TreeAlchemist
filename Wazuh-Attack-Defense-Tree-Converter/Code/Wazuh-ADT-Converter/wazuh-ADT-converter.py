ac335b0e38361efe5e3ac8ef9666303410d74237
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-10-31 15:59:33 +0100

    Succesfully obtain a Tree and print it as wazuh rules for single-nodes

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh-ADT-converter.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh-ADT-converter.py
@@ -5,13 +5,21 @@ and obtain the Wazuh implementation of it.
 '''
 import user_input_parser
 import ADT_xml_parser
+from TreeClasses.Tree import *
 
 
 def main():
 
-    tree_dir_path = user_input_parser.get_valid_tree_path() # The path to the directory containing the ADT files used for execution.
+    #tree_dir_path = user_input_parser.get_valid_tree_path() # The path to the directory containing the ADT files used for execution.
 
-    usable_ADT_structure = ADT_xml_parser.convert_xml_ADT_to_usable_structure(tree_dir_path = tree_dir_path)
+    # TEMP
+    tree_dir_path = r'C:\Users\fadra\Desktop\Github\TreeAlchemist\Wazuh-Attack-Defense-Tree-Converter\Code\Wazuh-ADT-Converter\Input-Files\test-tree'
+    # TEMP
+    usable_ADT_structure : Tree = ADT_xml_parser.convert_xml_ADT_to_usable_structure(tree_dir_path = tree_dir_path)
+
+    wazuh_rules_with_group_and_single_nodes_no_states = usable_ADT_structure.to_string_group_and_single_nodes()
+
+    print(wazuh_rules_with_group_and_single_nodes_no_states)
 
     
 
