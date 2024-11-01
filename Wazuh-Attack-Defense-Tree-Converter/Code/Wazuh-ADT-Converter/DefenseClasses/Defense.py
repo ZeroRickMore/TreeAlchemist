40a5c56421fbcac0cdb6298b3b81fafc8536ce04
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-01 23:22:14 +0100

    Create Defense.py

---
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/DefenseClasses/Defense.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# Import scripts from above folder
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from terminal_UI_utils import PrintUtils, ExitUtils
+import validations
+from DefenseClasses.Command import Command
+from DefenseClasses.ActiveResponse import ActiveResponse
+
+
+class Defense:
+    '''
+    This class represents the whole defense, so it is mapped to one or more states
+    and has a Command and Active response.
+    '''
+
+    def __init__(self, 
+                 #mapped_states : List[int] = None, # The states the defense is mapped to, inside of defense_to_nodes.json
+                 
+
+                ):
+        pass
