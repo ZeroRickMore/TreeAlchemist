5a1300044319c810f43e7a7e889785388018d135
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-03 12:25:00 +0100

    Continue the main converting

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh-ADT-converter.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh-ADT-converter.py
@@ -5,7 +5,9 @@ and obtain the Wazuh implementation of it.
 '''
 import user_input_parser
 import ADT_xml_parser
-from TreeClasses.Tree import *
+import defense_definition_xml_operations
+from TreeClasses.Tree import Tree
+import wazuh_ready_printer
 
 
 def main():
@@ -13,13 +15,21 @@ def main():
     #tree_dir_path = user_input_parser.get_valid_tree_path() # The path to the directory containing the ADT files used for execution.
 
     # TEMP
-    tree_dir_path = r'C:\Users\fadra\Desktop\Github\TreeAlchemist\Wazuh-Attack-Defense-Tree-Converter\Code\Wazuh-ADT-Converter\Input-Files\test-tree'
+    import os
+    tree_dir_path = os.path.join(os.getcwd(), r'Wazuh-Attack-Defense-Tree-Converter\Code\Wazuh-ADT-Converter\Input-Files\test-tree')
     # TEMP
-    usable_ADT_structure : Tree = ADT_xml_parser.convert_xml_ADT_to_usable_structure(tree_dir_path = tree_dir_path)
 
-    wazuh_rules_with_group_and_single_nodes_no_states = usable_ADT_structure.to_string_group_and_single_nodes()
+    ADT : Tree = ADT_xml_parser.convert_xml_ADT_to_usable_structure(tree_dir_path = tree_dir_path)
 
-    print(wazuh_rules_with_group_and_single_nodes_no_states)
+    wazuh_ready_atk_nodes_no_states : str = wazuh_ready_printer.to_string_all_attacks_single_nodes(ADT)
+
+    print(wazuh_ready_atk_nodes_no_states)
+
+    all_defenses = defense_definition_xml_operations.get_all_defenses(tree_dir_path = tree_dir_path)
+
+    wazuh_ready_def_nodes = wazuh_ready_printer.to_string_all_defenses_wazuh_ready(tree_name='COOLEST ADT', all_defenses=all_defenses, tab_times=0)
+
+    print(wazuh_ready_def_nodes)
 
     
 
