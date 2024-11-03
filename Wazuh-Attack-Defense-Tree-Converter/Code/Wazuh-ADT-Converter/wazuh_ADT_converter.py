c158d17e1c3fb4fc44b0a2bba20b416bb0f683b2
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-03 23:43:59 +0100

    Rename

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh_ADT_converter.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/wazuh_ADT_converter.py
@@ -8,6 +8,8 @@ import ADT_xml_parser
 import defense_definition_xml_parser
 from TreeClasses.Tree import Tree
 import wazuh_ready_printer
+import states_to_defense_parser
+import ultimate_generator
 
 
 def main():
@@ -25,11 +27,17 @@ def main():
 
     print(wazuh_ready_atk_nodes_no_states)
 
-    all_defenses = defense_definition_xml_parser.get_all_defenses(tree_dir_path = tree_dir_path)
+    all_defenses, id_to_defense = defense_definition_xml_parser.get_all_defenses(tree_dir_path = tree_dir_path)
 
     wazuh_ready_def_nodes = wazuh_ready_printer.to_string_all_defenses_wazuh_ready(tree_name='COOLEST ADT', all_defenses=all_defenses, tab_times=0)
 
     print(wazuh_ready_def_nodes)
+
+    all_states, state_id_to_state = states_to_defense_parser.get_all_states_to_defense(tree_dir_path=tree_dir_path)
+
+    print(states_to_defense_parser.to_string_all_states_pretty(all_states=all_states))
+    print(states_to_defense_parser.to_string_state_id_to_state_pretty(state_id_to_state=state_id_to_state))
+
 
     
 
