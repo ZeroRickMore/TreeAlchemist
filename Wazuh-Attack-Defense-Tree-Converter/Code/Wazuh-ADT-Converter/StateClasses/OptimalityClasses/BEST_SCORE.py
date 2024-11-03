ffff51a58338f2eeb451cf08abddc045e9e9bace
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-03 23:28:05 +0100

    Improve validations following template

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/StateClasses/OptimalityClasses/BEST_SCORE.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/StateClasses/OptimalityClasses/BEST_SCORE.py
@@ -1,9 +1,9 @@
-from AbstractOptimality import AbstractOptimality
 # Import scripts from above folder
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from terminal_UI_utils import PrintUtils, ExitUtils
+from StateClasses.OptimalityClasses.AbstractOptimality import AbstractOptimality
 
 class BEST_SCORE(AbstractOptimality):
     '''
@@ -15,16 +15,27 @@ class BEST_SCORE(AbstractOptimality):
     '''
 
     def validate_score(self):
-        return isinstance(self.get_property_value_attribute_no_duplicate_tags_traversed('score'), int)
+        return isinstance(self.get_score(), int)
     
     def validate_score_with_error_launch(self):
         if not self.validate_score():
-            score = self.get_property_value_attribute_no_duplicate_tags_traversed('score')
+            score = self.get_score()
             error_prefix = f"BEST_SCORE with score {score} failed validation on"
             error_suffix = f"was given instead."
             ExitUtils.exit_with_error(f'{error_prefix} <score>. It should be and int, but {score} of type {type(score)} {error_suffix}')
 
+    def set_instance_variables_from_properties(self):
+        score = self.get_property_value_text_no_duplicate_tags_traversed(property_key='score')
+        try:
+            self.score = int(score)
+        except: # The score is not a int number
+            ExitUtils.exit_with_error(f"The given score inside of <score> is {score} and it MUST be a int number.")
+
+    def get_score(self):
+        return self.score
+
     def validate_all(self):
+        self.set_instance_variables_from_properties()
         # Every validation for the properties.
         # "properties" is a dictionary, and all the methods to
         # use it are inside of the AbstractOptimality class.
