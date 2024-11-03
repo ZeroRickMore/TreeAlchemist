1cefb8ef6f48579a8a65d9fc345f77dfec269bf9
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-03 23:27:22 +0100

    Implement state_defense operations, validate_all, to_string

--- Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/StateClasses/State.py
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-ADT-Converter/StateClasses/State.py
@@ -1,9 +1,9 @@
-from StateClasses.StateDefense import StateDefense
 # Import scripts from above folder
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from terminal_UI_utils import PrintUtils, ExitUtils
+from StateClasses.StateDefense import StateDefense
 
 class State:
     '''
@@ -37,6 +37,7 @@ class State:
     def validate_description(self) -> bool:
         if not isinstance(self.get_description(), str):
             return False
+        return True
 
     def validate_description_with_error_launch(self):
         if not self.validate_description():
@@ -92,3 +93,65 @@ class State:
 
         if self.print_diagnostics:
             PrintUtils.print_in_green(f'- Inside <state>, <nodes> has been succesfully set to {node_ids}')
+
+
+
+    # ============================================
+    # <state_defense> operations
+    # ============================================
+    
+    def get_state_defense(self) -> StateDefense:
+        return self.state_defense
+
+    def validate_state_defense(self) -> bool:
+        if not isinstance(self.get_state_defense(), StateDefense):
+            return False
+        self.get_state_defense().validate_all()
+        return True
+
+    def validate_state_defense_with_error_launch(self):
+        if not self.validate_state_defense():
+            error_prefix = f"The State with state_defense [ {self.get_state_defense()} ] failed validation on"
+            error_suffix = f"was given instead."
+            ExitUtils.exit_with_error(f'{error_prefix} <state_defense> in <state>. {State.get_state_defense_allow_criteria()} {self.get_state_defense()} of type {type(self.get_state_defense())} {error_suffix}')
+
+    @staticmethod
+    def get_state_defense_allow_criteria() -> str:
+        return "It must be a StateDefense object."
+
+    def set_state_defense(self, state_defense : StateDefense) -> None:
+        self.state_defense = state_defense
+
+        self.validate_state_defense_with_error_launch()
+
+        if self.print_diagnostics:
+            PrintUtils.print_in_green(f'- Inside <state>, <state_defense> has been succesfully set to {state_defense}')
+
+
+
+
+    # ============================================
+    # Validate All
+    # ============================================   
+
+
+    def validate_all(self):
+
+        self.validate_description_with_error_launch()
+
+        self.validate_node_ids_with_error_launch()
+
+        self.validate_state_defense_with_error_launch()
+
+
+
+
+
+
+    def to_string(self, tab_times : int = 0):
+        give_tabs = '\t'*tab_times
+        return f"{give_tabs}- State ==========================\n\n{give_tabs}\tDesc: {self.get_description()}{give_tabs}\n\tNodes: {self.get_node_ids()}{give_tabs}\n{self.get_state_defense().to_string(tab_times=tab_times+1)}"
+    
+
+
+
