293d60b51eabda02984fed8893628737a7c2034b
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-06 23:31:05 +0100

    Create wazuh_adtmanagerd.py

---
+++ Wazuh-Attack-Defense-Tree-Converter/Code/Wazuh-adtmanagerd/wazuh_adtmanagerd.py
@@ -0,0 +1,11 @@
+'''
+Script to handle the defense launch, based off the current ADT state.
+
+It uses the same State and Tree classes used in TreeAlchemist,
+and maps each agent to a list of states based on the ADT.
+Realistically, it is a dictionary from {ADT_id (int) : state (list[int])
+
+This manager gets the alerts raised from syslog
+
+The folder ADT_generation_files contains a set of text files that help recreate the tree and nodes very simply
+'''
