dac6ec8169523e4d8c56c6d4b994b554550131b1
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-09 18:34:57 +0100

    Generate guide

--- TreeAlchemist/Code/ADT-Generator/write_on_files.py
+++ TreeAlchemist/Code/ADT-Generator/write_on_files.py
@@ -33,9 +33,13 @@ class FilesWriter:
         lowest_rule_id = find_lowest_rule_id(all_rules_as_string)
         file_name = f'{lowest_rule_id}-TreeAlchemized-{root_name}.xml'
 
-        with open(os.path.join(tree_folder, file_name), "w") as f:
+        full_rules_file = os.path.join(tree_folder, file_name)
+
+        with open(full_rules_file, "w") as f:
             f.write(all_rules_as_string)
 
+        self.rules_file = full_rules_file
+        self.tree_folder = tree_folder
         self.has_generated_something = True
 
 
@@ -53,7 +57,36 @@ class FilesWriter:
                 ExitUtils.exit_with_error(f"The folder {root_name} already exist, cannot generate the files.\nIf you want to generate the files, rename or remove the folder.")
 
         file_name = f'Command-Activeres-{root_name}_manually_put_into_ossec_conf.xml'
-        with open(os.path.join(tree_folder, file_name), "w") as f:
+
+        full_defs_file = os.path.join(tree_folder, file_name)
+        with open(full_defs_file, "w") as f:
             f.write(all_defenses_as_string)
 
+        self.def_file = full_defs_file
+        self.tree_folder = tree_folder
         self.has_generated_something = True
+
+    
+    def create_daemon_readable_file(self, all_states_to_defenses):
+        pass
+
+
+    def generate_guide_file(self):
+        self.rules_file # Il file con le regole
+        self.def_file # Il file con command e active-reponse
+        self.tree_folder # La folder che contiene tutti questi subfiles
+        string = ''
+
+        string += 'Hello, and welcome to the TreeAlchemist guide!\nThis is an extremely short text to guide you through the upload of these files inside of Wazuh.\n\n'
+
+        string += f'Rules File=======================\n\t- Go into /var/ossec/etc/rules/\n\t- Move the rules file {self.rules_file} inside of it, and job done!\n\n'
+
+        string += f'Command Active-Response File=======================\n\t- Modify content of /var/ossec/etc/ossec.conf\n\t- Copypaste all of the content of {self.def_file} inside of it, at the very end, and job done!\n\n'
+
+
+        string += '\nI hope everything worked for you!\nHave fun with your tree, and do not forget to launch the daemon in order to make the tree handling functional!'
+
+
+        guide_file = os.path.join(self.tree_folder, 'README.txt')
+        with open(guide_file, "w") as f:
+            f.write(string)
