32b89e1122fbe5e310abd3022dc46ae35e023306
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-10 19:49:31 +0100

    Partially implement defense command launch

--- TreeAlchemist/Code/Daemons/wazuh_adtmanagerd.py
+++ TreeAlchemist/Code/Daemons/wazuh_adtmanagerd.py
@@ -38,6 +38,10 @@ import read_toml
 import ADT_daemon_readable_txt_parser
 import wazuh_adtmanagerd_utils
 from pprint import pprint
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime, timedelta
+
 
 
 debug = True
@@ -48,6 +52,14 @@ print("==========Gathering the Trees==========\n\n")
 this_script_dir = os.path.dirname(__file__)
 log_file_path = os.path.join(this_script_dir, 'Logs', 'wazuh_adtmanagerd.log')
 tree_name_to_structure_dict = ADT_daemon_readable_txt_parser.parse_all_daemon_readable_files(os.path.join(this_script_dir, 'Trees'))
+
+api_username = read_toml.get_api_username()
+api_pwd = read_toml.get_api_pwd()
+
+JWT_token = None
+JWT_refresh_time = read_toml.get_JWT_refresh_time() # Time in seconds after which the JWT token expires
+JWT_expire_timestamp = None
+
 
 
 print("==========================================================================================\nThis is the dictionary that was gathered. Feel free to ignore it.\n==========================================================================================\n\n")
@@ -81,7 +93,7 @@ def process_new_alert():
     should_I_run_defense = update_tree_state(alert_infos)
     # Raise defenses if a state is matched
     if should_I_run_defense:
-        which_defenses_ran_and_wazuh_api_response = run_defense_if_present(alert_infos)
+        which_defenses_ran_and_wazuh_api_response = run_defenses_if_present(alert_infos)
 
     if not should_I_run_defense:
         return jsonify({"status": "success", "message": f"Alert {alert_infos['rule_id']} was already raised, doing nothing."}), 200
@@ -123,7 +135,7 @@ def update_tree_state(alert_infos) -> bool:
     return True
 
 
-def run_defense_if_present(alert_infos) -> dict[str, dict]:
+def run_defenses_if_present(alert_infos):
     '''
     Returns a dict of the defenses that were executed mapped to the raw response of Wazuh API .
     '''
@@ -141,25 +153,95 @@ def run_defense_if_present(alert_infos) -> dict[str, dict]:
                 print(f"DEBUG: A defense has been matched for state [ {state} ] in ADT = [ {tree_name} ]. Trying to launch it.")
 
             app.logger.info(f'A defense has been matched for state [ {state} ] in ADT = [ {tree_name} ]. Trying to launch it.')
-            return launch_defense_commands(defense_command=curr_tree_states_that_have_a_defense[state])
-
+            return launch_defense_commands(defense_commands=curr_tree_states_that_have_a_defense[state], agent=alert_infos['agent'])
+            
+    curr_state = curr_tree_structure_dict['current_state']
     if debug:
-        print(f"DEBUG: No defenses found for state [ {curr_tree_structure_dict['current_state']} ] in ADT = [ {tree_name} ]. Doing nothing.")
+        print(f"DEBUG: No defenses found for state [ {curr_state} ] in ADT = [ {tree_name} ]. Doing nothing.")
 
-    app.logger.info(f'No defenses found for state [ {curr_tree_structure_dict['current_state']} ] in ADT = [ {tree_name} ]. Doing nothing.')
+    app.logger.info(f'No defenses found for state [ {curr_state} ] in ADT = [ {tree_name} ]. Doing nothing.')
 
     return {}
 
 
-def launch_defense_commands(defense_command : str) -> dict[str, dict]:
+def launch_defense_commands(defense_commands : list[str], agent : str) -> dict[str, dict]:
     # Insert the logic to manage the PUT inside of WazuhServer API.
     '''
     Executes the defense commands, and returns a dictionary with defense -> raw response of Wazuh API .
     '''
+    defense_to_status = {}
+    for defense in defense_commands:
+        set_wazuh_jwt_token() # Making extra sure the token is valid. We really want that defense to work, after all !
 
-    pass
+        if defense in defense_to_status.keys():
+            app.logger.warning(f"The defense {defense} is duplicated in one of the states, running it only once.")
+            continue
 
+        defense_to_status[defense] = launch_single_defense(defense=defense, agent=agent)
     
+    return defense_to_status
+
+
+def launch_single_defense(defense : str, agent : str):
+    # Replace these placeholders with actual values
+    command = defense
+
+    # URL with query parameters
+    url = f"https://127.0.0.1:55000/agents?agents_list={agent}"
+
+    # JSON body
+    payload = {
+        "command": command
+    }
+
+    # Headers with Bearer token
+    headers = {
+        "Authorization": f"Bearer {JWT_token}",
+        "Content-Type": "application/json"
+    }
+
+    # Send PUT request
+    response = requests.put(url, json=payload, headers=headers, verify=False)
+
+    code = response.status_code
+
+    if code == 200:
+        app.logger.info(f"Defense {defense} launched with success on agent {agent}")
+    else:
+        app.logger.error(response.json())
+
+    # Print the response text
+    return response.json()
+
+
+def set_wazuh_jwt_token():
+    global JWT_expire_timestamp, JWT_token
+
+    if JWT_refresh_time is not None and JWT_token is not None:
+        # We want to be extremely sure that the JWT token is valid, hence we want a window of at least 20 seconds
+        # to utilize it
+        if datetime.now() < JWT_expire_timestamp - timedelta(seconds=20):
+            return
+
+    # Replace with your username and password
+    username = api_username
+    password = api_pwd
+
+    # URL for authentication
+    url = "https://127.0.0.1:55000/security/user/authenticate"
+
+    # Disable SSL verification (similar to `-k` in curl)
+    response = requests.post(url, auth=HTTPBasicAuth(username, password), verify=False)
+    try:
+        response.raise_for_status()
+    except Exception as e:
+        app.logger.error(f"THE API CREDENTIALS ARE WRONG OR THERE IS AN ERROR WITH THE WAZUH API: [ {e} ]")
+    
+    JWT_expire_timestamp = datetime.now() + timedelta(seconds=JWT_refresh_time)
+    app.logger.info("Requested a new JWT token to Wazuh API succesfully.")
+    # Return JWT
+    JWT_token = response.json()['data']['token']
+
 
 
 @app.route('/', methods=['GET'])
