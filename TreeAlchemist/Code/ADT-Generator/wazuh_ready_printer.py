facd848ae255a9b10d6dc190a2e89bd88846087b
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-10 15:15:56 +0100

    Implement to_string_node_rule_ids_to_defense_id

--- TreeAlchemist/Code/ADT-Generator/wazuh_ready_printer.py
+++ TreeAlchemist/Code/ADT-Generator/wazuh_ready_printer.py
@@ -1,8 +1,11 @@
 from typing import List, Union
 
 from TreeClasses.Tree import Tree
+from TreeClasses.TreeNode import TreeNode
 from DefenseClasses.Defense import Defense
 from DefenseClasses.DefensesTogether import DefensesTogether
+from StateClasses.State import State
+
 
 def to_string_all_attacks_single_nodes(ADT : Tree):
     return ADT.to_string_group_and_single_nodes()
@@ -35,3 +38,48 @@ def to_string_all_defenses_wazuh_ready(tree_name : str = 'unspecified', all_defe
 
 
 
+def to_string_node_rule_ids_to_defense_id(all_states : List[State],
+                                          node_id_to_node : dict[int, TreeNode]
+                                          ):
+    string = ''
+
+    for state in all_states:
+        curr_node_ids_string = '['
+        for id in state.get_node_ids():
+            curr_node_ids_string += str(node_id_to_node[id].get_informations().get_wazuh_rule_config().get_rule_id()) + ','
+        
+        curr_node_ids_string = curr_node_ids_string[:-1]
+        curr_node_ids_string += '] :: ['
+        # We add a zero at the end because Wazuh API for some reason appends a 0 at the end of the command name to trigger it...
+        curr_defense =  state.get_state_defense().get_defense()
+        if isinstance(curr_defense, Defense):
+            curr_node_ids_string += state.get_state_defense().get_defense().get_comm_name() + '0'
+        else:
+            defenses = curr_defense.get_defenses()
+            for defense in defenses:
+                curr_node_ids_string += defense.get_comm_name() + '0,'
+            curr_node_ids_string = curr_node_ids_string[:-1]
+        
+        curr_node_ids_string += ']' 
+        string += curr_node_ids_string + '\n'
+    
+    return string
+
+
+
+def to_string_tree_for_daemon_read( adt : Tree,
+                                    all_states : List[State],
+                                    node_id_to_node : dict[int, TreeNode]
+                                    ):
+    
+    string = '===START OF ADT===\n'
+
+    string += adt.print_adt_name_with_node_ids()
+
+    string += '\n'
+
+    string += to_string_node_rule_ids_to_defense_id(all_states=all_states, node_id_to_node=node_id_to_node)
+
+    string += '===END OF ADT===\n'
+
+    return string
