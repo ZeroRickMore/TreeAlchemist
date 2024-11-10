d4703ce4fc2e2ff8d61de704cbc9b743d6f86005
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-10 19:49:12 +0100

    Update read_toml.py

--- TreeAlchemist/Code/Daemons/read_toml.py
+++ TreeAlchemist/Code/Daemons/read_toml.py
@@ -1,10 +1,34 @@
 import toml
 import os
 
+# Get stuff from daemon_varaibles.toml
 toml_file_path = os.path.join(os.path.dirname(__file__), 'daemon_variables.toml')
 config = toml.load(toml_file_path)
 
 toml_port = config['settings']['port']
 
+# Get credentials
+toml_file_path = os.path.join(os.path.dirname(__file__), 'api_credentials.toml')
+config = toml.load(toml_file_path)
+
+toml_username = config['username']
+toml_pwd = config['password']
+
+
+# Get api settings
+toml_file_path = os.path.join(os.path.dirname(__file__), 'api_settings.toml')
+config = toml.load(toml_file_path)
+
+toml_JWT_refresh_time = int(config['JWT_refresh_time'])
+
 def get_port():
     return toml_port
+
+def get_api_username():
+    return toml_username
+
+def get_api_pwd():
+    return toml_pwd
+
+def get_JWT_refresh_time() -> int:
+    return toml_JWT_refresh_time
