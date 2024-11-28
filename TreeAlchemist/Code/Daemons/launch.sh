8d1f3b8e79a04093a20a72a917b65494ca656f8a
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2024-11-28 12:19:10 +0100

    Make the opened window bigger for better printing (it's cool)

--- TreeAlchemist/Code/Daemons/launch.sh
+++ TreeAlchemist/Code/Daemons/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Open the first terminal and run the first Python script
-gnome-terminal -- bash -c "python3 ./Observer.py; exec bash"
+gnome-terminal --geometry=100x24 -- bash -c "python3 ./Observer.py; exec bash"
 
 # Open the second terminal and run the second Python script
-gnome-terminal -- bash -c "python3 ./Athanor.py; exec bash"
+gnome-terminal --geometry=100x24 -- bash -c "python3 ./Athanor.py; exec bash"
